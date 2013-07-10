6c170ed87bfb04fd5de1b2c9026dfc37ec799b64
Tim Pietrusky <timpietrusky@googlemail.com>
2013-07-10 21:25:25 +0200

    Changed favicon

--- page/header.php
+++ page/header.php
@@ -3,7 +3,7 @@
 <head>
     <title>Cantact.me | NSA-free — powered by NSA!</title>
     <link href="style.css" rel="stylesheet" type="text/css"/>
-    <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="img/cantact.ico" type="image/x-icon" />
     <meta charset="utf-8">
     <meta name="robots" content="index,follow">
     <meta name="viewport" content="width=device-width,initial-scale=1">
@@ -19,7 +19,7 @@
 <div class="wrapper">
 
     <header>
-        <img src="img/favicon.ico" width="24" class="logo" alt="K"/>
+        <img src="img/cantact.ico" width="24" class="logo" alt="K"/>
         <h1>
             <a href="./">Cantact.me</a> <small>NSA-free &mdash; powered by NSA!</small>
         </h1>
