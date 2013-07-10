7fbca2265f884ec6b2beb7fd4a33c277665ceed3
Felix De Montis <felix.demontis@gmail.com>
2013-07-10 22:09:02 +0200

    minor changes

--- page/header.php
+++ page/header.php
@@ -22,7 +22,7 @@
 <div class="wrapper">
 
     <header>
-        <img src="img/cantact_200.jpg" width="100" class="logo" alt="K"/>
+        <img src="img/cantact_200.jpg" width="100" class="logo" alt="C"/>
         <h1>
             <a href="./">Cantact.me</a> <small>NSA-free &mdash; powered by NSA!</small>
         </h1>
