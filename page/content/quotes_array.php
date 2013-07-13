e5daf151569f6ccac44fe48261464dac3f0ed745
WebGate <lucas.bonomi@gmail.com>
2013-07-13 19:04:37 +0200

    Adding a Quotes_array.php
    To add quotes easily for everyone.

---
+++ page/content/quotes_array.php
@@ -0,0 +1,16 @@
+<?php 
+	$quotes = array(
+  'When <b>Gad</b> created earth, he used <a href="http://cantact.me">cantact.me</a>', 
+  '<a href="http://cantact.ft/chat.php">Nouveau ! Discutez a l\'ambre des regards...</a>', 
+  'Alle <b>kantaktieren</b> sich weltweit!! Willkammen im <b>Kantakte-Netzwerk</b>',
+  'The King says: <b>Cantacts</b> are <b>impartants</b> far <b>everyane</b>!',
+  'Avec <b>Cantact</b> faites vous des cantacts avec tact!',
+  'Au commencement, il y eut <a href="http://cantact.fr">cantact.fr</a>',
+  'A dead cantact is a bad cantact.',
+  'What U dan\'t knaw yet ? We gat a <a href="http://kantakte.de/kantaktroulette.html">KantaktRoulette</a> !',
+  'Quai t\'es pas encore au caurant ? An a aussi une !<a href="http://kantakte.de/kantaktroulette.html">CantactRoulette</a> !',
+  'It is a bird? It is a plane? Na, it\'s <b>Super-Cantact</b> in the Sky.',
+  '<b>Chuck Norris</b> invented Cantact.me while playing all the main casts in <br> Back to the Future, Star Wars & Pulp Fiction.'
+);
+
+?>
